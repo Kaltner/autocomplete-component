f631b80fcb5b8de53a3dc333f2ed984067229ab0
Danilo Kaltner <danilokaltner@Danilos-MacBook-Pro.local>
2021-01-06 21:48:21 -0800

    Creted the bulma CSS files

--- src/AutoComplete/index.tsx
+++ src/AutoComplete/index.tsx
@@ -1,7 +1,7 @@
-import { FC } from 'react';
+import { FC } from "react";
 
 const AutoComplete: FC = () => {
+  return <div></div>;
+};
 
-}
-
-export default AutoComplete
+export default AutoComplete;
