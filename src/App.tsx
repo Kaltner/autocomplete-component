ba6e29d95d191c7bfc09f0be7396233a71ba0dd7
Danilo Kaltner <danilokaltner@Danilos-MacBook-Pro.local>
2021-01-07 23:54:29 -0800

    Created the debounced field and mocked the cities

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import { FC } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import "./Bulma.scss";
+import "./AutoComplete";
+import AutoComplete from "./AutoComplete";
 
 const App: FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <AutoComplete></AutoComplete>;
 };
 
 export default App;
--- src/AutoComplete/index.tsx
+++ src/AutoComplete/index.tsx
@@ -1,7 +1,77 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
+interface City {
+  name: string;
+}
 
 const AutoComplete: FC = () => {
-  return <div></div>;
+  const [cities, setCities] = useState([]);
+
+  const mockCities: City[] = [
+    { name: "Vancouver" },
+    { name: "New Westminster" },
+    { name: "Burnaby" },
+    { name: "Richmond" },
+    { name: "Delta" },
+    { name: "Whistler" },
+    { name: "North Vancouver" },
+    { name: "Abbsfort" },
+  ];
+
+  const debounce = useDebouncedCallback((value) => handleDebounce(value), 500);
+
+  const fetchCities = (query: string): City[] => {
+    // TODO: Turn this into case insensitive
+    return mockCities.filter((v) => v.name.includes(query));
+  };
+
+  const handleDebounce = (q: string) => {
+    const foundCities = fetchCities(q);
+    setCities(foundCities as []);
+    console.log("Found Cities", cities);
+  };
+
+  const renderCitiesFound = () => {
+    // TODO: Think about an empty state for the cities.
+    return (
+      <div className="dropdown-menu" id="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {cities.map((city: City) => {
+            return (
+              <a href="#" className="dropdown-item">
+                {city.name}
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+
+  const renderInput = () => {
+    return (
+      <div className="container">
+        <div className={cities.length > 0 ? "dropdown is-active" : "dropdown"}>
+          <div className="dropdown-trigger">
+            <div className="field">
+              <label className="label">Name</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Find a city!"
+                  onChange={(e) => debounce.callback(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+          {renderCitiesFound()}
+        </div>
+      </div>
+    );
+  };
+
+  return renderInput();
 };
 
 export default AutoComplete;
